ec81cb1f4435969386e3f182fd19cac69378f0d7
Beiming Zhang <thebeimingzhang@outlook.com>
2024-12-09 02:37:15 +0800

    Add student test framework

--- test/studenttests.ml
+++ test/studenttests.ml
@@ -13,6 +13,33 @@ open Driver
 (* Use this file to create additional test cases here to help you   *)
 (* debug your comiplper                                             *)
 
-let student_local_tests : suite = [
-  
-] 
+let my_unit_tests =
+  [ ( "subtype: string <: string?"
+    , fun () ->
+        if Typechecker.subtype Tctxt.empty (TRef RString) (TNullRef RString)
+        then ()
+        else failwith "should not fail" )
+  ; ( "no subtype: string? </: string"
+    , fun () ->
+        if Typechecker.subtype Tctxt.empty (TNullRef RString) (TRef RString)
+        then failwith "should not succeed"
+        else () )
+  ]
+;;
+
+let my_tests =
+  [ "test/studentprograms/correct_global_struct.oat"
+  ]
+;;
+
+let my_error_tests =
+  [ "test/studentprograms/error_global_struct.oat"
+  ]
+;;
+
+let student_local_tests : suite =
+  [ Test ("my unit tests", my_unit_tests)
+  ; Test ("my tests", typecheck_file_correct my_tests)
+  ; Test ("my error tests", typecheck_file_error my_error_tests)
+  ]
+;;
