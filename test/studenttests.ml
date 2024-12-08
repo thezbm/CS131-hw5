0c4a7d79ca0cf2150fc2d7caedd25fba6db02dc3
Beiming Zhang <thebeimingzhang@outlook.com>
2024-12-08 19:47:20 +0800

    Initial commit

---
+++ test/studenttests.ml
@@ -0,0 +1,18 @@
+open Testlib
+open Util.Assert
+open X86
+open Ll
+module Driver = Oat.Driver
+module Backend = Oat.Backend
+module Typechecker = Oat.Typechecker
+module Frontend = Oat.Frontend
+module Tctxt = Oat.Tctxt
+open Backend
+open Driver
+
+(* Use this file to create additional test cases here to help you   *)
+(* debug your comiplper                                             *)
+
+let student_local_tests : suite = [
+  
+] 
